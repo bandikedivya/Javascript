3658311f52c66ffbd8decac8ab24d97486d9bb3a
bandikedivya <divyacrkdv@gmail.com>
2025-07-26 21:48:18 +0530

    Functions and its types

--- 4.FucntionsTypes/1.AnonymousFunc.js
+++ 4.FucntionsTypes/1.AnonymousFunc.js
@@ -4,26 +4,26 @@
 //size of this function is less than the other[named] func, so it is easy to understand.
 
 //Defining the Anonymous function  [this is variable function or function expression.]
-// var getName = function(){
-//     let MyName = "Divya Bandike";
-//     console.log(MyName);
-// }
+var getName = function(){
+    let MyName = "Divya Bandike";
+    console.log(MyName);
+}
 
 // //calling the function
-// getName();
+ getName();
 
 
 
 // //Adding Parameters
 
-// var StudNames = function(StudName1, StudName2,StudName3)
-// {
-//     console.log(StudName1);
-//     console.log(StudName2);
-//     console.log(StudName3);
-// }
+var StudNames = function(StudName1, StudName2,StudName3)
+{
+    console.log(StudName1);
+    console.log(StudName2);
+    console.log(StudName3);
+}
 
-// StudNames("Divya", "Sreeparna", "Shalini");
+StudNames("Divya", "Sreeparna", "Shalini");
 
 
 //IIFE Func
@@ -47,37 +47,37 @@
 //Callback Func
 
 
-// //function alreadydefined functionname(parameter){
-// //parameter(); }  //passed parameter with method signature must
-// function sayHello(callbackfn){
-//     callbackfn();
-// }
+//function alreadydefined functionname(parameter){
+//parameter(); }  //passed parameter with method signature must
+function sayHello(callbackfn){
+    callbackfn();
+}
 
 
 
-// //functionname(Anonymous function)  //It is defined in this way.
-// sayHello(function(){
-//     console.log("Hello");
-// });
+//functionname(Anonymous function)  //It is defined in this way.
+sayHello(function(){
+    console.log("Hello");
+});
 
 
 //Passing parameters
 
 //function alreadydefined functionname(parameter){
 //parameter(); }  //passed parameter with method signature must
-// function sayHello(callbackfn){
-//     callbackfn(10,20); //adding agruments here
+function sayHello(callbackfn){
+    callbackfn(10,20); //adding agruments here
 
-// }
+}
 
 
 
-// //functionname(Anonymous function)  //It is defined in this way.
-// sayHello(function(a,b){
-//     console.log("Hello");
-//     console.log(a);
-//     console.log(b);
-// });
+//functionname(Anonymous function)  //It is defined in this way.
+sayHello(function(a,b){
+    console.log("Hello");
+    console.log(a);
+    console.log(b);
+});
 
 
 //Predefined functions

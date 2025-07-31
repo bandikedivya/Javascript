d3c9c279c47fb18073f3c99ee324021df4650656
bandikedivya <divyacrkdv@gmail.com>
2025-08-01 00:19:55 +0530

    added comparison operator code

---
+++ ComparisonNdLogicalOperator/comparisonoperator.js
@@ -0,0 +1,44 @@
+// Comparison operator
+
+//Executes the block based on the condition 
+
+
+//in condition how to use operators
+/*  comparison operator
+     (== , != ,  > , >=, < , <= , ===) 
+     Prints  weather true or false.
+*/
+
+
+console.log(5 == 5);  //true
+console.log( 1  == 5);  //false
+console.log(2 > 5); //false
+console.log(2 >= 5) ; //false
+console.log(5 < 10) ; //true
+console.log(8 <=  10); //true
+// ===
+
+
+// Type coercison 
+
+//conversion of one type to another type 
+console.log( 1 == "1");  //js will convert the string 1 ["1"] to number and then compare the value. // returns true.
+console.log(2 =="2"); //returns true.
+
+// === --> it will check the value and the datatype as well.
+console.log( 1 === "1"); // returns false.
+console.log("Adam" === "Adam");  //returns true.
+console.log("Adam" === 12); //returns false
+
+
+// using comparison operator, will creat if condition [conditional statement]
+let student_marks = 55;
+if(student_marks>65)
+{
+     console.log("%c ✅ 🎉  Student passed the exam", "color: green");
+}
+else if(student_marks<65)
+{
+     console.log("%c ❌ 😡 Student has not failed the exam", "color: red");
+}
+
---
+++ ComparisonNdLogicalOperator/logicaloperators.js
@@ -0,0 +1,42 @@
+// Logical operator
+
+
+// Logical AND (&&)
+let stud_name = "Divya"; 
+let stud_age = 25;
+console.log(stud_name == "Divya" && stud_age == 25); //true  && true  - true
+console.log(stud_name == "Divya" && stud_age == 21); //true  && false - false
+console.log(stud_name == "Kavya" && stud_age == 25); //false && true  - false
+console.log(stud_name == "Kavya" && stud_age == 21); //false && false - false
+
+if(stud_name == "divya" && stud_age == 25)
+{
+    console.log(" %c ✅ Student exists in the List", "color:green");
+}
+else
+{
+    console.log("%c  ❌ Student does not exists in the List", "color:red");
+}
+
+
+
+// Logical OR (||)
+console.log("-------------------Logical OR----------------------------")
+let stud_name1 = "Divya"; 
+let stud_age1 = 25;
+console.log(stud_name == "Divya" || stud_age == 25); //true  || true  - true
+console.log(stud_name == "Divya" || stud_age == 21); //true  || false - true
+console.log(stud_name == "Kavya" || stud_age == 25); //false || true  - true
+console.log(stud_name == "Kavya" || stud_age == 21); //false || false - false
+
+//if(stud_name == "Divya" || stud_age == 25) // true -- prints if block.
+//if(stud_name == "Divya" || stud_age == 21) // true -- prints if block.
+//if(stud_name == "Kavya" || stud_age == 25) //true -- prints if block.
+if(stud_name == "Kavya" || stud_age == 21) // false -- prints else block.
+{
+    console.log(" %c ✅ Student exists in the List", "color:green");
+}
+else
+{
+    console.log("%c  ❌ Student does not exists in the List", "color:red");
+}
